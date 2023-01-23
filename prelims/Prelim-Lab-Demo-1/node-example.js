9abdca7c94aa9c35df11bddcbe77f2352b8e85a5
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-01-23 12:48:07 +0800

    Add files via upload

---
+++ prelims/Prelim-Lab-Demo-1/node-example.js
@@ -0,0 +1,14 @@
+//Estanislao, Francilyn Bernadette P.
+//Module: 3
+//Week 2 and 3: Basics of NodeJS
+
+//first program
+console.log("Welcome to WCSERVER!");
+
+
+//Creating my first function
+function Display(x){
+    console.log(x);
+}
+
+Display(143)
