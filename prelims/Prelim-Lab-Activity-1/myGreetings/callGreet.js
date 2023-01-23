7f9eb609e6fe4bd2f50e8678317d3cc8f5084d3b
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-01-23 22:17:14 +0800

    Update callGreet.js

--- prelims/Prelim-Lab-Activity-1/myGreetings/callGreet.js
+++ prelims/Prelim-Lab-Activity-1/myGreetings/callGreet.js
@@ -1,4 +1,7 @@
+//Estanislao, Francilyn Bernadette P
+//WD-201
+
 var greetings = require('./greet.js');
 
-console.log(greetings("John"));
-console.log(greetings("Jane"));
+console.log(greetings('John'));
+console.log(greetings('Jane'));
