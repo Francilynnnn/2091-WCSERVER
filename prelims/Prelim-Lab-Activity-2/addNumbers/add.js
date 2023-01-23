08bbc948b838f3a24b73244ca2d832866b24a621
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-01-23 23:26:34 +0800

    Add files via upload

---
+++ prelims/Prelim-Lab-Activity-2/addNumbers/add.js
@@ -0,0 +1,10 @@
+//Estanislao, Francilyn Bernadette P.
+//Laboratory Activity 2: Adding Numbers
+//WD 201
+
+function add(num1,num2) {
+    let x = num1 + num2;
+    return x;
+}
+
+module.exports = add;
---
+++ prelims/Prelim-Lab-Activity-2/addNumbers/app.js
@@ -0,0 +1,7 @@
+//Estanislao, Francilyn Bernadette P.
+//Laboratory Activity 2: Adding Numbers
+//WD 201
+
+const add = require ('./add.js');
+
+console.log("The sum of the two numbers is " + add(15,21));
