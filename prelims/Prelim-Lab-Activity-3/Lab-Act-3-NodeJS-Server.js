27f1418eefc19d5b6f0cec8ed6b047a5faf7c87c
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-01-30 13:04:44 +0800

    1 file added, 1 deleted

--- prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
+++ prelims/Prelim-Lab-Activity-3/Lab-Act-3-NodeJS-Server.js
@@ -11,21 +11,21 @@ var server = http.createServer(function (req, res) {
       '<html><body><h1>Welcome to my Node.js Application</h1></body></hmtl>'
     );
     res.write(
-      '<html><body><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></hmtl>'
+      '<html><body><p>Welcome Francilyn Estanislao. This is an activity about basics of Node.js</p></body></hmtl>'
     );
     res.end();
   } else if (req.url == '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><h2>This is the About Page</h2></body></hmtl>');
     res.write(
-      '<html><body><p>Hello John Smith. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></hmtl>'
+      '<html><body><p>Hello Francilyn Estanislao. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></hmtl>'
     );
     res.end();
   } else if (req.url == '/contact') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><h3>This is the About Page</h3></body></hmtl>');
     res.write(
-      '<html><body><p>John Smith, if you want additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs-tutorials</p></body></hmtl>'
+      '<html><body><p>Francilyn Estanislao, if you want additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs-tutorials</p></body></hmtl>'
     );
     res.end();
   } else if (req.url == '/gallery') {
