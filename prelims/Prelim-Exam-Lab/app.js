317f07f060e516364f054bea5efad687a818ec6d
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-02-08 15:49:54 +0800

    2 files added

---
+++ prelims/Prelim-Exam-Lab/app.js
@@ -0,0 +1,9 @@
+//Estanislao, Francilyn Bernadette P
+//Prelim Hands-on Exam
+
+var greetings = require('./function.js');
+var Name = 'Francilyn';
+
+console.log(greetings.greet(Name));
+console.log(greetings.congrats(Name));
+console.log(greetings.goodbye(Name));
---
+++ prelims/Prelim-Exam-Lab/function.js
@@ -0,0 +1,16 @@
+//Estanislao, Francilyn Bernadette P
+//Prelim Hands-on Exam
+
+var greetings = {
+  greet: function (Name) {
+    return 'Hello ' + Name + '!';
+  },
+  congrats: function (Name) {
+    return 'Congratulations, ' + Name + '!';
+  },
+  goodbye: function (Name) {
+    return 'Goodbye ' + Name + '!';
+  },
+};
+
+module.exports = greetings;
