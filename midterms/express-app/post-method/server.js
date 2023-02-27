c37d81ec4f2e7f5e61a555b0c4526ea830a72485
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-02-27 12:57:23 +0800

    7 files added

---
+++ midterms/express-app/post-method/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+//require body-parser for this method
+var bodyParser = require('body-parser');
+
+//Create application/x-www-form-urlencoded parser
+var urlecondedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.post('/process_post', urlecondedParser, function (req, res) {
+  response = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+  };
+
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
