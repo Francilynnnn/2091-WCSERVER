a0bcfb2f8e2a84b0a207406ff7a5b8a1148de21b
Francilynnnn <123344393+Francilynnnn@users.noreply.github.com>
2023-03-13 12:58:30 +0800

    3 files modified, 2 added

--- midterms/index.js
+++ midterms/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
+// set the view engine to Pug
+app.set('view engine', 'ejs');
+
+// serve static files
+app.use(express.static('public'));
+
+// render the index page
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {
+    title: 'My Personal Profile',
+    name: 'Francilyn Bernadette P. Estanislao',
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
